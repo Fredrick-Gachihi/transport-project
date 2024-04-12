de125d5bfd2b801d91b599d858888f236681a96e
fredrick-gachihi <fredrickgachihi24@gmail.com>
2024-04-12 13:55:49 +0300

    my transport project

--- main.js
+++ main.js
@@ -1,56 +1,9 @@
 
-// function fetchStopData(searchTerm) {
-//     const url = `https://v6.vbb.transport.rest/locations?query=${searchTerm}`;
-  
-//     fetch(url)
-//       .then(response => response.json())
-//       .then(data => {
-//         if (data.length === 0) {
-//           displayError('No stops found for this search term.');
-//         } else {
-//           displayStops(data);
-//         }
-//       })
-//       .catch(error => {
-//         console.error(error);
-//         displayError('An error fetching data. Please check your network connection or API endpoint.');
-//       });
-//   }
-  
-//   function displayStops(stopData) {
-//     const locationsList = document.getElementById('locations');
-//     locationsList.innerHTML = ''; // Clear existing content
-  
-//     stopData.forEach(stop => {
-//         const listItem = document.createElement('li');
-//         listItem.textContent =`Stop Name: ${stop.name}  (Products: ${stop.products.join(', ')})`;
-//         locationsList.appendChild(listItem);
-//       });
-//     }
-
-//   const submitButton = document.querySelector('.submit');//
-//   submitButton.addEventListener('click', () => {
-//     const searchTerm = document.querySelector('.input').value.trim();
-//     if (!searchTerm) {
-//       displayError('Please enter a search term.');
-//       return;
-//     }
-  
-//     fetchStopData(searchTerm);
-//   });
-
-//   function displayError(message) {
-//     const errorMessageElement = document.getElementById('error-message');
-//     errorMessageElement.textContent = message; 
-//     console.log(error-message);
-// }
-
-
 function fetchStopData(searchTerm) {
   const url = `https://v6.vbb.transport.rest/locations?query=${searchTerm}`;
 
   fetch(url)
-    .then(response => response.json())
+    .then(response => response.json())//to return the responce in json
     .then(data => {
       if (data.length === 0) {
         displayError('No stops found for this search term.');
@@ -75,11 +28,10 @@ function displayStops(stopData) {
       if (Array.isArray(stop.products)) {
         listItem.textContent = `Stop Name: ${stop.name}  (Stop Products: ${stop.products.join(', ')})`;
       } else {
-        listItem.textContent = `Stop Name: ${stop.name} (Products: Not Available)`;
+        listItem.textContent = `Stop Name: ${stop.name} (Products: Available)`;//if there is an available means of transport
       }
     } else {
-
-      listItem.textContent = `Stop Name: ${stop.name} (Products: Not Available)`;
+        listItem.textContent = `Stop Name: ${stop.name} (Products: Not Available)`;//if there is no means of transport
     }
 
     locationsList.appendChild(listItem);
